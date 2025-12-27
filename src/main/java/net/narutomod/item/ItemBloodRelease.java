683427e98c365579beb5884883e4162f585a57f1
davidsonw564-dotcom <davidsonw564@gmail.com>
2025-12-27 15:41:08 -0600

    Update ItemBloodRelease.java

--- src/main/java/net/narutomod/item/ItemBloodRelease.java
+++ src/main/java/net/narutomod/item/ItemBloodRelease.java
@@ -5,81 +5,214 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.util.ResourceLocation;
 
 import net.narutomod.ElementsNarutomodMod;
-import net.narutomod.item.ItemJutsu;
 import net.narutomod.creativetab.TabModTab;
+import net.narutomod.item.ItemJutsu;
 import net.narutomod.entity.EntityBloodDragon;
 import net.narutomod.entity.EntityBloodPrison;
 import net.narutomod.entity.EntityHidingInBloodMist;
 import net.narutomod.entity.EntityBloodSiphonChains;
-import net.narutomod.capabilities.NarutoModVariables;
+import net.narutomod.entity.EntityRendererRegister;
 
 @ElementsNarutomodMod.ModElement.Tag
 public class ItemBloodRelease extends ElementsNarutomodMod.ModElement {
+
     @ObjectHolder("narutomod:blood_release")
     public static final Item block = null;
 
+    public ItemBloodRelease(ElementsNarutomodMod instance) {
+        super(instance, 999); // ID for Blood Release
+    }
+
+    /*
+     * ======================
+     * BLOOD RELEASE JUTSU
+     * ======================
+     */
     public static final ItemJutsu.JutsuEnum BLOODDRAGON =
         new ItemJutsu.JutsuEnum(0, "blood_dragon", 'S', 200d, new EntityBloodDragon.EC.Jutsu());
+
     public static final ItemJutsu.JutsuEnum BLOODPRISON =
         new ItemJutsu.JutsuEnum(1, "blood_prison", 'S', 150d, new EntityBloodPrison.EC.Jutsu());
+
     public static final ItemJutsu.JutsuEnum HIDINGINBLOODMIST =
         new ItemJutsu.JutsuEnum(2, "hiding_in_blood_mist", 'A', 80d, new EntityHidingInBloodMist.EC.Jutsu());
+
     public static final ItemJutsu.JutsuEnum BLOODSIPHONCHAINS =
         new ItemJutsu.JutsuEnum(3, "blood_siphon_chains", 'A', 100d, new EntityBloodSiphonChains.EC.Jutsu());
 
-    public ItemBloodRelease(ElementsNarutomodMod instance) {
-        super(instance, 999);
-    }
-
     @Override
     public void initElements() {
-        this.elements.items.add(() -> new ItemCustom(
-            BLOODDRAGON, BLOODPRISON, HIDINGINBLOODMIST, BLOODSIPHONCHAINS
-        ));
+        this.elements.items.add(() -> new ItemCustom(BLOODDRAGON, BLOODPRISON, HIDINGINBLOODMIST, BLOODSIPHONCHAINS)
+            .setRegistryName("blood_release"));
     }
 
     @SideOnly(Side.CLIENT)
     public void registerModels(ModelRegistryEvent event) {
-        ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("narutomod:blood_release", "inventory"));
+        ModelLoader.setCustomModelResourceLocation(block, 0,
+            new ModelResourceLocation("narutomod:blood_release", "inventory"));
     }
 
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        new Renderer().register();
+    }
+
+    /*
+     * ======================
+     * BLOOD RELEASE ITEM
+     * ======================
+     */
     public static class ItemCustom extends ItemJutsu.Base {
+
         private final ItemJutsu.JutsuEnum[] myJutsuList;
 
         public ItemCustom(ItemJutsu.JutsuEnum... list) {
             super(ItemJutsu.JutsuEnum.Type.BLOOD, list);
-            this.myJutsuList = list;
             this.setUnlocalizedName("blood_release");
             this.setRegistryName("blood_release");
             this.setCreativeTab(TabModTab.tab);
-
-            // Example cooldowns (20 ticks = 1 second)
-            this.defaultCooldownMap[BLOODDRAGON.index] = 400;       // 20 seconds
-            this.defaultCooldownMap[BLOODPRISON.index] = 300;       // 15 seconds
-            this.defaultCooldownMap[HIDINGINBLOODMIST.index] = 160; // 8 seconds
-            this.defaultCooldownMap[BLOODSIPHONCHAINS.index] = 200; // 10 seconds
+            this.myJutsuList = list;
         }
 
         @Override
         public void onCreated(ItemStack stack, World world, EntityPlayer player) {
-            if (!world.isRemote) {
-                for (ItemJutsu.JutsuEnum j : myJutsuList) {
-                    NarutoModVariables.PlayerVariables.get(player).unlockedJutsu.add(j);
-                }
+            for (ItemJutsu.JutsuEnum j : myJutsuList) {
+                super.unlockJutsu(player, j); // use Base method
             }
         }
 
         @Override
-        public void onUpdate(ItemStack stack, World world, net.minecraft.entity.Entity entity, int slot, boolean held) {
+        public void onUpdate(ItemStack stack, World world, Entity entity, int slot, boolean held) {
             super.onUpdate(stack, world, entity, slot, held);
+        }
+    }
+
+    /*
+     * ======================
+     * BLOOD RELEASE RENDERER
+     * ======================
+     */
+    public static class Renderer extends EntityRendererRegister {
+
+        @SideOnly(Side.CLIENT)
+        @Override
+        public void register() {
+            RenderingRegistry.registerEntityRenderingHandler(EntityBloodDragon.class, manager -> new RenderBloodDragon(manager));
+            RenderingRegistry.registerEntityRenderingHandler(EntityBloodPrison.class, manager -> new RenderBloodPrison(manager));
+            RenderingRegistry.registerEntityRenderingHandler(EntityHidingInBloodMist.class, manager -> new RenderBloodMist(manager));
+            RenderingRegistry.registerEntityRenderingHandler(EntityBloodSiphonChains.class, manager -> new RenderSiphonChains(manager));
+        }
+
+        @SideOnly(Side.CLIENT)
+        public static class RenderBloodDragon extends Render<EntityBloodDragon> {
+            private final ResourceLocation texture = new ResourceLocation("narutomod:textures/blood_dragon.png");
+            protected final ModelBase model = new ModelBase();
+
+            public RenderBloodDragon(RenderManager renderManager) {
+                super(renderManager);
+                this.shadowSize = 0.5f;
+            }
+
+            @Override
+            public void doRender(EntityBloodDragon entity, double x, double y, double z, float entityYaw, float pt) {
+                GlStateManager.pushMatrix();
+                GlStateManager.translate(x, y, z);
+                this.bindEntityTexture(entity);
+                GlStateManager.popMatrix();
+            }
+
+            @Override
+            protected ResourceLocation getEntityTexture(EntityBloodDragon entity) {
+                return this.texture;
+            }
+        }
+
+        @SideOnly(Side.CLIENT)
+        public static class RenderBloodPrison extends Render<EntityBloodPrison> {
+            private final ResourceLocation texture = new ResourceLocation("narutomod:textures/blood_prison.png");
+            protected final ModelBase model = new ModelBase();
+
+            public RenderBloodPrison(RenderManager renderManager) {
+                super(renderManager);
+                this.shadowSize = 0.5f;
+            }
+
+            @Override
+            public void doRender(EntityBloodPrison entity, double x, double y, double z, float entityYaw, float pt) {
+                GlStateManager.pushMatrix();
+                GlStateManager.translate(x, y, z);
+                this.bindEntityTexture(entity);
+                GlStateManager.popMatrix();
+            }
+
+            @Override
+            protected ResourceLocation getEntityTexture(EntityBloodPrison entity) {
+                return this.texture;
+            }
+        }
+
+        @SideOnly(Side.CLIENT)
+        public static class RenderBloodMist extends Render<EntityHidingInBloodMist> {
+            private final ResourceLocation texture = new ResourceLocation("narutomod:textures/blood_mist.png");
+            protected final ModelBase model = new ModelBase();
+
+            public RenderBloodMist(RenderManager renderManager) {
+                super(renderManager);
+                this.shadowSize = 0.5f;
+            }
+
+            @Override
+            public void doRender(EntityHidingInBloodMist entity, double x, double y, double z, float entityYaw, float pt) {
+                GlStateManager.pushMatrix();
+                GlStateManager.translate(x, y, z);
+                this.bindEntityTexture(entity);
+                GlStateManager.popMatrix();
+            }
+
+            @Override
+            protected ResourceLocation getEntityTexture(EntityHidingInBloodMist entity) {
+                return this.texture;
+            }
+        }
+
+        @SideOnly(Side.CLIENT)
+        public static class RenderSiphonChains extends Render<EntityBloodSiphonChains> {
+            private final ResourceLocation texture = new ResourceLocation("narutomod:textures/blood_siphon_chains.png");
+            protected final ModelBase model = new ModelBase();
+
+            public RenderSiphonChains(RenderManager renderManager) {
+                super(renderManager);
+                this.shadowSize = 0.5f;
+            }
+
+            @Override
+            public void doRender(EntityBloodSiphonChains entity, double x, double y, double z, float entityYaw, float pt) {
+                GlStateManager.pushMatrix();
+                GlStateManager.translate(x, y, z);
+                this.bindEntityTexture(entity);
+                GlStateManager.popMatrix();
+            }
+
+            @Override
+            protected ResourceLocation getEntityTexture(EntityBloodSiphonChains entity) {
+                return this.texture;
+            }
         }
     }
 }
