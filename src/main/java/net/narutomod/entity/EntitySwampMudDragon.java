a9c52613cbf074064d12e1de8a35275699d20e27
davidsonw564-dotcom <davidsonw564@gmail.com>
2026-01-27 09:13:15 -0600

    Update EntitySwampMudDragon.java

--- src/main/java/net/narutomod/entity/EntitySwampMudDragon.java
+++ src/main/java/net/narutomod/entity/EntitySwampMudDragon.java
@@ -88,3 +88,82 @@ public class EntitySwampMudDragon extends ElementsNarutoMod.ModElement {
 			this.yOrigin = shooter.posY;
 		}
     
+	private void setWaitPosition() {
+			if (this.shootVec != null) {
+				ProcedureUtils.Vec2f v2f = ProcedureUtils.getYawPitchFromVec(this.shootVec);
+				this.setRotation(v2f.x, v2f.y);
+			} else if (this.shootingEntity != null) {
+				Vec3d vec = this.shootingEntity instanceof EntityLiving && ((EntityLiving)this.shootingEntity).getAttackTarget() != null
+				 ? ((EntityLiving)this.shootingEntity).getAttackTarget().getPositionVector().subtract(this.getPositionVector())
+				 : ProcedureUtils.objectEntityLookingAt(this.shootingEntity, 50d).hitVec.subtract(this.getPositionVector());
+				ProcedureUtils.Vec2f v2f = ProcedureUtils.getYawPitchFromVec(vec);
+				this.setRotation(v2f.x, v2f.y);
+			}
+			this.motionY = this.ticksAlive <= this.wait / 2 ? 3d * this.getEntityScale() / (double)this.wait * 2d : 0.0d;
+		}
+
+		/*private void updateLimbSwing() {
+			this.prevLimbSwingAmount = this.limbSwingAmount;
+	        double d5 = this.posX - this.prevPosX;
+	        double d7 = this.posZ - this.prevPosZ;
+	        double d9 = this.posY - this.prevPosY;
+	        float f10 = MathHelper.sqrt(d5 * d5 + d9 * d9 + d7 * d7) * 4.0F;
+	        if (f10 > 1.0F) {
+	            f10 = 1.0F;
+	        }
+	        this.limbSwingAmount += (f10 - this.limbSwingAmount) * 0.4F;
+	        this.limbSwing += this.limbSwingAmount;
+		}*/
+
+		@Override
+		public void onUpdate() {
+			if (this.prevHeadYaw == 0.0f && this.prevHeadPitch == 0.0f) {
+				this.prevHeadYaw = this.rotationYaw;
+				this.prevHeadPitch = this.rotationPitch;
+			}
+			super.onUpdate();
+			if (!this.world.isRemote && (this.ticksAlive > 100 || this.shootingEntity == null || !this.shootingEntity.isEntityAlive())) {
+				this.setDead();
+			} else {
+				if (this.ticksAlive <= this.wait) {
+					this.lastVec = this.getPositionVector();
+					this.setWaitPosition();
+					//this.setEntityScale(this.fullScale * MathHelper.clamp((float)this.ticksAlive / (float) this.wait, 0.1F, 1.0F));
+				} else if (!this.isLaunched()) {
+					if (this.shootVec != null) {
+						this.shoot(this.shootVec.x, this.shootVec.y, this.shootVec.z, 0.95f, 0f);
+					} else if (this.shootingEntity != null) {
+						Vec3d vec = this.shootingEntity instanceof EntityLiving && ((EntityLiving)this.shootingEntity).getAttackTarget() != null
+						 ? ((EntityLiving)this.shootingEntity).getAttackTarget().getPositionVector().subtract(this.getPositionVector())
+						 : ProcedureUtils.objectEntityLookingAt(this.shootingEntity, 50d).hitVec.subtract(this.getPositionVector());
+						this.shoot(vec.x, vec.y, vec.z, 0.95f, 0f);
+					}
+				}
+				this.updateSegments();
+				//this.updateLimbSwing();
+				this.prevHeadYaw = this.rotationYaw;
+				this.prevHeadPitch = this.rotationPitch;
+			}
+		}
+
+		public void updateSegments() {
+			Vec3d cposvec = this.getPositionVector();
+			float slength = this.getEntityScale() * 11.0F * 0.0625F;
+			ProcedureUtils.Vec2f vec = new ProcedureUtils.Vec2f(this.rotationYaw, this.rotationPitch)
+			 .subtract(this.prevRotationYaw, this.prevHeadPitch);
+			Vec3d vec4 = cposvec.subtract(this.lastVec);
+			double d4 = vec4.lengthVector();
+//String s = ">>> ["+(this.world.isRemote?"client":"server")+"], vec="+vec+", moved:"+d4;
+			if (d4 >= slength && this.ticksAlive > this.wait) {
+				this.partRot.add(0, vec);
+				int i = 1;
+				for ( ; i < (int)(d4 / slength); i++) {
+					this.partRot.add(0, ProcedureUtils.Vec2f.ZERO);
+				}
+				this.lastVec = vec4.normalize().scale(slength * i).add(this.lastVec);
+			} else {
+				this.partRot.set(0, this.partRot.get(0).add(vec));
+			}
+//System.out.println(s+", partRot:"+this.partRot);
+		}
+	  
