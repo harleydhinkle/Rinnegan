fa5b4b623b334d6a3432d848c1edcd764c0d0cc0
davidsonw564-dotcom <davidsonw564@gmail.com>
2026-01-27 08:52:35 -0600

    Update ItemSwampRelease.java
    starting to write code. Need to create jutsu first

--- src/main/java/net/narutomod/item/ItemSwampRelease.java
+++ src/main/java/net/narutomod/item/ItemSwampRelease.java
@@ -1,1 +1,12 @@
+package net.narutomod.item;
+
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraft.item.Item;
+import net.narutomod.creativetab.TabModTab;
+import net.narutomod.ElementsNarutomodMod;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 
